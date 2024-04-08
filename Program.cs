1b1b21b27f5aa0cde24806aa209b62fd3d473ee4
Sumedh Bhalerao <80387460+Sumedh-Bhalerao@users.noreply.github.com>
2024-04-08 11:40:28 +0530

    Add files via upload

---
+++ Program.cs
@@ -0,0 +1,12 @@
+﻿internal class Program
+{
+    private static void Main(string[] args)
+    {
+        Console.WriteLine("Hello, World!");
+        var x = () => Console.WriteLine("I am a Lambda Funct");
+        x();
+
+        var square = (int x) => x * x;
+        Console.WriteLine(square(100));
+    }
+}
