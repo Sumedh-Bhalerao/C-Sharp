7f7068066667928f4a9dac3ebedd90629a19f3c1
Sumedh Bhalerao <80387460+Sumedh-Bhalerao@users.noreply.github.com>
2024-04-08 12:46:12 +0530

    Add files via upload

---
+++ ClassLibrary1/Class1.cs
@@ -0,0 +1,16 @@
+﻿using System;
+using System.Net.Security;
+using InheritanceDemo;
+namespace ClassLibrary1
+{
+    public class Class1: Employee1
+    {
+        public void test()
+        {
+            eid = 123;
+            ename = "Emp1";
+            dept = "Marketing";
+            Console.WriteLine("Hello World");
+        }
+    }
+}
