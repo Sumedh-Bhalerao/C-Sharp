f91abd9c6ad9aa0951fd9dd0c036f983573fe488
Sumedh Bhalerao <80387460+Sumedh-Bhalerao@users.noreply.github.com>
2024-04-08 13:09:56 +0530

    Add files via upload

---
+++ Program.cs
@@ -0,0 +1,34 @@
+﻿internal class Program
+{
+	private static void Main(string[] args)
+	{
+		int a, b, c;
+		string opt;
+		Console.WriteLine("enter a,b");
+		a = Convert.ToInt32(Console.ReadLine());
+		b = Convert.ToInt32(Console.ReadLine());
+		Console.WriteLine("+ Addition\n - Substraction\n * Multiplication\n / Division");
+		Console.WriteLine("Select Your Option");
+		opt = Console.ReadLine();
+
+		switch(opt)
+		{
+			case "+":
+				c = a + b;
+				Console.WriteLine(c);
+				break;
+			case "-":
+				c = a - b;
+				Console.WriteLine(c);
+				break;
+			case "*":
+				c = a * b;
+				Console.WriteLine(c);
+				break;
+			case "/":
+				c = a / b;
+				Console.WriteLine(c);
+				break;
+		}
+	}
+}
