d517bd7642faae68156c9f5b70a7040a46e1f93a
Sumedh Bhalerao <80387460+Sumedh-Bhalerao@users.noreply.github.com>
2024-04-08 12:47:26 +0530

    Add files via upload

---
+++ BaseDemo/Employee1.cs
@@ -0,0 +1,24 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace BaseDemo
+{
+    internal class Employee1
+    {
+        string Fname, Lname;
+        int salary;
+        public Employee1() { 
+        
+        }
+        public Employee1(string Fname,string Lname,int salary) { 
+        this.Fname = Fname;
+        this.Lname = Lname;
+        this.salary = salary;
+            Console.WriteLine("Employee Info");
+            Console.WriteLine(Fname + " " + Lname + "" + base ,salary, "" + salary);
+        }
+    }
+}
