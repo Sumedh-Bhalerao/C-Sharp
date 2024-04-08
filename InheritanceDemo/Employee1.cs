8e2a0c5e2b2cacab4e7691fd0cfb897ef38337ca
Sumedh Bhalerao <80387460+Sumedh-Bhalerao@users.noreply.github.com>
2024-04-08 12:43:39 +0530

    Add files via upload

---
+++ InheritanceDemo/Employee1.cs
@@ -0,0 +1,16 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Transactions;
+
+namespace InheritanceDemo
+{
+    public class Employee1
+    {
+        public int eid;
+        protected string ename;
+        protected internal string dept;
+    }
+}
